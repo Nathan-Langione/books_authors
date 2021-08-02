c4164b5c270afb29a6f44af57f7a81b8d4e93411
Nathan Langione <nlangione@wayfair.com>
2021-08-02 11:41:08 -0400

    completed assignment

--- books_authors_app/models.py
+++ books_authors_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"User object: {self.title} {self.desc} ({self.id})"
+
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    notes = models.TextField()
+    books = models.ManyToManyField(Books, related_name="author")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"User object: {self.first_name} {self.last_name} {self.notes} ({self.id})"
--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Books, Authors
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "all_authors": Authors.objects.all()    
+    }
+    return render(request,'index.html', context)
